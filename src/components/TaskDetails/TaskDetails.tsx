b6ef63f289ece2d412fbf0e6218c9b81f15df09a
GiulianoCharra <65138966+GiulianoCharra@users.noreply.github.com>
2023-11-26 01:11:34 -0300

    borrado ejemplo

--- src/components/TaskDetails/TaskDetails.tsx
+++ src/components/TaskDetails/TaskDetails.tsx
@@ -8,7 +8,7 @@ import { useNavigate } from "react-router-dom";
 import { useToast } from "../../contexts/ToastProvider.js";
 import ModalConfirmDeleteTask from "../ModalConfirDeleteTask/ModalConfirmDeleteTask.js";
 
-export function TaskDetails({task, taskremove, tastupdate}) {
+export function TaskDetails() {
   const { selectedTask: task } = useTaskContext();
   const [estado, setEstado] = useState<TaskEstado>(task?.estado || TaskEstado.POR_HACER);
 
